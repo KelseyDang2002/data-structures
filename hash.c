e7dd361a7e29a354442be926483a8a70f3a3af94
KelseyDang2002 <kelseydang813@gmail.com>
2025-01-11 22:08:34 -0800

    hash function

--- hash.c
+++ hash.c
@@ -19,7 +19,14 @@ typedef struct Node {
 } Node;
 
 // node constructor
-struct Node* initNode(struct Node* node, char* key, char* data) {};
+struct Node* initNode(struct Node* node, char* key, char* data) {
+    node->key = key;
+    node->data = data;
+    
+    // set next ptr to NULL since initially it is not pointing to any other node yet
+    node->next = NULL;
+    return;
+}
 
 // hash map data structure
 typedef struct HashMap {
@@ -29,29 +36,77 @@ typedef struct HashMap {
 } HashMap;
 
 // hash map constructor
-struct HashMap* initHashMap(struct HashMap* map) {};
+struct HashMap* initHashMap(struct HashMap* map) {
+    map->capacity = MAX_CAPACITY; // capacity of the hash map
+    map->currNumElements = 0;
+
+    /*
+    array of size 1
+
+    allocate memory for the array of pointers that represent the hash map's buckets
+    struct Node** - an array of pointers to Node struct, each pointer represents the head
+                    of a linked list for a bucket
+    sizeof(struct Node*) - size of a single pointer to a Node
+    map->capacity - total number of buckets in the hash map
+    malloc() - allocates enough memory for all bucket pointers
+    */
+    map->arr = (struct Node**)malloc(sizeof(struct Node*) * map->capacity);
+    return;
+}
 
 /*
 method to compute the index with a hash function
 
 this returns a bucket index based on the input key
 */
-int hashFunction(struct hashMap* map, char* key) {};
+int hashFunction(struct HashMap* map, char* key) {
+    int bucketIndex;
+
+    // accumulates the hash values as the function iterates through the characters of the key
+    int sum = 0;
+
+    /*
+    a multiplier that gives higher weight to characters at later positions in the string
+    initialized to 31, a commonly used small prime number in hash functions 
+    */
+    int factor = 31;
+
+    // loop through each character of the key string
+    for(int i = 0; i < strlen(key); i++) {
+        /*
+        update sum
+
+        sum = sum + (ascii value of char * (primeNumber ^ x))...
+        where x = 1, 2, 3 ... n
+        */
+        sum = ((sum % map->capacity) + (((int)key[i]) * factor) % map->capacity) % map->capacity;
+
+        /*
+        update factor
+
+        factor = factor * prime number...(prime number) ^ x
+        */
+        factor = ((factor % __INT16_MAX__) * (31 % __INT16_MAX__)) % __INT16_MAX__;
+    }
+
+    bucketIndex = sum; // assign final computed hash value to bucket index
+    return bucketIndex;
+}
 
 /*
 method to insert data into hash map
 */
-void insert(struct hashMap* map, char* key, char* data) {};
+void insert(struct HashMap* map, char* key, char* data) {};
 
 /*
 method to delete data from hash map
 */
-void delete(struct hashMap* map, char* key) {};
+void delete(struct HashMap* map, char* key) {};
 
 /*
 method to search for data in the hash map
 */
-char* search(struct hashMap* map, char* key) {};
+char* search(struct HashMap* map, char* key) {};
 
 // main
 int main(int argc, char* argv[]) {
